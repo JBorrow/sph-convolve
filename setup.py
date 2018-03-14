5983caba6377f7e78265711cea96617c7ff8f8a4
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-03-14 11:40:28 +0000

    Added packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sphconvolve",
+    version="0.1",
+    description="An SPH convolution module",
+    url="https://github.com/jborrow/sph-convolve",
+    author="Josh Borrow",
+    author_email="joshua.borrow@durham.ac.uk",
+    packages=["sphconvolve"],
+    zip_safe=False
+)
